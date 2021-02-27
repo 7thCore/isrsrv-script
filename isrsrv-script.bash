8d656ec1c87c92c7ba808d15eeb2408351f6ae32
7thCore <34578834+7thCore@users.noreply.github.com>
2021-02-27 21:32:15 +0100

    Update isrsrv-script.bash

--- isrsrv-script.bash
+++ isrsrv-script.bash
@@ -2,7 +2,7 @@
 
 #Interstellar Rift server script by 7thCore
 #If you do not know what any of these settings are you are better off leaving them alone. One thing might brake the other if you fiddle around with it.
-export VERSION="202012090456"
+export VERSION="202102272132"
 
 #Basics
 export NAME="IsRSrv" #Name of the tmux session
